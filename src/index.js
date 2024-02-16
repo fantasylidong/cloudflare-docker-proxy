66f712ba06b7fb4cc09a5b6419feb2fc74591e75
morzlee <fantasylidong@gmail.com>
2024-02-17 00:01:06 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.trygek.com": "https://registry-1.docker.io",
+  "quay.trygek.com": "https://quay.io",
+  "gcr.trygek.com": "https://gcr.io",
+  "k8s-gcr.trygek.com": "https://k8s.gcr.io",
+  "k8s.trygek.com": "https://registry.k8s.io",
+  "ghcr.trygek.com": "https://ghcr.io",
+  "cloudsmith.trygek.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
